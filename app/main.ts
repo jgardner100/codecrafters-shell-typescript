9ca088e14a54cb3461cf63103fbb6655fb9e206a
John Gardner <jgardner100@hotmail.com>
2026-05-04 16:18:47 +1000

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -11,6 +11,11 @@ rl.prompt();
 rl.on("line", (input: string) => {
   const command = input.trim();
 
+  if (command === "exit") {
+    rl.close();
+    process.exit(0);
+  }
+
   console.log(`${command}: command not found`);
 
   rl.prompt();
